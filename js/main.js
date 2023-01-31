58711d03f663ff07dcc5a8009b4cd43af94bfaa2
thathsarani shenali <shenalithath@gmail.com>
2023-01-31 15:19:54 +0530

    added home page changes

--- js/main.js
+++ js/main.js
@@ -1,25 +1,19 @@
-
-
-      (function () {
-      'use strict'
+(function () {
+   'use strict'
       const forms = document.querySelectorAll('.requires-validation')
       Array.from(forms).forEach(function (form) {
-            form.addEventListener('submit', function (event) {
+      form.addEventListener('submit', function (event) {
             if (!form.checkValidity()) {
                   event.preventDefault()
                   event.stopPropagation()
             }
             form.classList.add('was-validated')
       }, false)
-      })
-      })()
+})
+})()
 
-
-      // // for image load
-      // const voyageImage = document.querySelectorAll('.voyageImage')
-      // $(voyageImage).load(function() {
-	// 	// Animate loader off screen
-	// 	$(".se-pre-con").fadeOut("slow");;
-	// });
-
+$('.nav-link').on('click',function() {
+      $('.navbar-collapse').collapse('hide');
+});
       
+// navbar icon change animation
